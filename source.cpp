0634398e1fec11dcde45e9a2c1ba10bc7e2cfd92
S-Y <sy-unimportant@outlook.com>
2022-03-11 22:51:12 +0000

    created initial documents

---
+++ source.cpp
@@ -0,0 +1,18 @@
+#include "source.h"
+#include <iostream>
+
+int main()
+{
+
+    // Account management.
+    //  > Create.
+    //  > Delete.
+    //  > Modify.
+    // Deposit amount.
+    // Withdraw amount.
+    // Show balance.
+    // List accounts.
+
+    std::cout << "Hello there" << std::endl;
+    return 0;
+}
